70f7a75aabefde649d7371c703b0cf468a703c2d
Denis Pankov <idenispankov@gmail.com>
2020-10-21 14:36:46 -0700

    utils added

--- scripts/index.js
+++ scripts/index.js
@@ -1,77 +1,29 @@
 import {toggleModal} from './utils.js'; 
 import FormValidator from './FormValidator.js'; 
 import Card from './Card.js'; 
- 
-// Initial Cards Array  
-const initialCards = [  
-  {  
-    name: "Yosemite Valley",  
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"  
-  },  
-  {  
-    name: "Lake Louise",  
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"  
-  },  
-  {  
-    name: "Bald Mountains",  
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"  
-  },  
-  {  
-    name: "Latemar",  
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg"  
-  },  
-  {  
-    name: "Vanoise National Park",  
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"  
-  },  
-  {  
-    name: "Lago di Braies",  
-    link: "https://code.s3.yandex.net/web-code/lago.jpg"  
-  }  
-];  
-  
- 
-// Modal Windows  
-const editProfileModal = document.querySelector('.modal_type_edit-profile');  
-const addCardModal = document.querySelector('.modal_type_add-card');  
-const imageModalWindow = document.querySelector('.modal_type_image');  
-  
-// Open Buttons  
-const editProfileButton = document.querySelector('.profile__edit-button');  
-const addCardButton = document.querySelector('.profile__add-button');  
-  
-// Submit Form Buttons  
-const editProfileForm = editProfileModal.querySelector('.form_type_profile');  
-const addCardForm = addCardModal.querySelector('.form_type_card');  
-const modalFigure = imageModalWindow.querySelector('.modal__figure');  
-  
-// Close Buttons  
-const closeProfileButton = editProfileModal.querySelector('.form__close-button');  
-const closeAddCardFormButton = addCardModal.querySelector('.form__close-button');  
-const closeCardImageButton = modalFigure.querySelector('.form__close-button_type-image');  
-  
-// Edit Profile Form Inputs  
-const inputName = editProfileForm.querySelector('.form__input_type_name');  
-const inputAbout = editProfileForm.querySelector('.form__input_type_about');  
-  
-const profileName = document.querySelector('.profile__text');  
-const profileAbout = document.querySelector('.profile__paragraph');  
-  
-// Add Card Form Inputs  
-const inputTitle = addCardForm.querySelector('.form__input_type_card-title');  
-const inputUrl = addCardForm.querySelector('.form__input_type_card-url');  
+import {
+  initialCards,
+  defaultConfig,
+  cardsList,
+  editProfileModal,
+  addCardModal,
+  imageModalWindow,
+  editProfileButton,
+  addCardButton,
+  editProfileForm,
+  addCardForm,
+  closeProfileButton,
+  closeAddCardFormButton,
+  closeCardImageButton,
+  inputName,
+  inputAbout,
+  inputTitle,
+  inputUrl
+} from '../utils/constants.js'; 
  
 /////////////////////////////////////////////////////////////////////////// 
  
 // New Form Validators 
-const defaultConfig = { 
-  formSelector: ".form", 
-  inputSelector: ".form__input", 
-  submitButtonSelector: ".form__button", 
-  inactiveButtonClass: "form__button_disabled", 
-  inputErrorClass: "form__input_type_error", 
-  errorClass: "form__error_visible" 
-}; 
  
 const editFormValidator = new FormValidator(defaultConfig, editProfileForm); 
 const addFormValidator = new FormValidator(defaultConfig, addCardForm); 
@@ -107,14 +59,11 @@ closeCardImageButton.addEventListener('click', () => {
   toggleModal(imageModalWindow);  
 });  
   
-// Cards List 
-const list = document.querySelector('.elements__list');  
-  
 // Render Card 
 function renderCard(data) {  
   const card = new Card(data, '.card-template'); 
   const cardElement = card.createCard(); 
-  list.prepend(cardElement); 
+  cardsList.prepend(cardElement); 
 }  
   
 // // Initial Cards Data  
---
+++ utils/constants.js
@@ -0,0 +1,70 @@
+// Initial Cards Array  
+export const initialCards = [  
+  {  
+    name: "Yosemite Valley",  
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"  
+  },  
+  {  
+    name: "Lake Louise",  
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"  
+  },  
+  {  
+    name: "Bald Mountains",  
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"  
+  },  
+  {  
+    name: "Latemar",  
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"  
+  },  
+  {  
+    name: "Vanoise National Park",  
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"  
+  },  
+  {  
+    name: "Lago di Braies",  
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"  
+  }  
+]; 
+
+// Default Configs
+export const defaultConfig = { 
+  formSelector: ".form", 
+  inputSelector: ".form__input", 
+  submitButtonSelector: ".form__button", 
+  inactiveButtonClass: "form__button_disabled", 
+  inputErrorClass: "form__input_type_error", 
+  errorClass: "form__error_visible" 
+}; 
+
+// Cards List 
+export const cardsList = document.querySelector('.elements__list');
+
+// Modal Windows  
+export const editProfileModal = document.querySelector('.modal_type_edit-profile');  
+export const addCardModal = document.querySelector('.modal_type_add-card');  
+export const imageModalWindow = document.querySelector('.modal_type_image'); 
+
+// Open Buttons  
+export const editProfileButton = document.querySelector('.profile__edit-button');  
+export const addCardButton = document.querySelector('.profile__add-button');  
+  
+// Submit Form Buttons  
+export const editProfileForm = editProfileModal.querySelector('.form_type_profile');  
+export const addCardForm = addCardModal.querySelector('.form_type_card');
+export const modalFigure = imageModalWindow.querySelector('.modal__figure');
+  
+// Close Buttons  
+export const closeProfileButton = editProfileModal.querySelector('.form__close-button');  
+export const closeAddCardFormButton = addCardModal.querySelector('.form__close-button');  
+export const closeCardImageButton = modalFigure.querySelector('.form__close-button_type-image');  
+  
+// Edit Profile Form Inputs  
+export const inputName = editProfileForm.querySelector('.form__input_type_name');  
+export const inputAbout = editProfileForm.querySelector('.form__input_type_about');  
+  
+export const profileName = document.querySelector('.profile__text');  
+export const profileAbout = document.querySelector('.profile__paragraph');  
+  
+// Add Card Form Inputs  
+export const inputTitle = addCardForm.querySelector('.form__input_type_card-title');  
+export const inputUrl = addCardForm.querySelector('.form__input_type_card-url');  
