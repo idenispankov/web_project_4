fbf7805aea2cbc2c983b6791bf74b5fdd5ef0624
Denis Pankov <idenispankov@gmail.com>
2020-10-21 15:55:05 -0700

    PopupWithForm.js added

---
+++ components/PopupWithForm.js
@@ -0,0 +1,31 @@
+import Popup from './Popup.js';
+
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector, handleSubmit) {
+    super(popupSelector);
+
+    this._handleSubmit = handleSubmit;
+    this._setEventListeners();
+    this._inputs = this._popupItem.querySelectorAll('.form__input');
+  }
+
+  _getInputsValue() {
+    this._formValues = {};
+    this._inputList.forEach(input => 
+    this._formValues[input.name] = input.value);
+    return this._formValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._popupItem.addEventListener('submit', (e) => {
+      e.preventDefault();
+      this._handleSubmit(this._getInputsValue());
+    });
+  }
+
+  close() {
+    this._popupItem.reset();
+    super.close();
+  };
+}
