5eefb03469b9d204ba1ede237a411e36b14892cd
Denis Pankov <idenispankov@gmail.com>
2020-10-21 16:11:16 -0700

    UserInfo.js added

---
+++ components/UserInfo.js
@@ -0,0 +1,15 @@
+export default class UserInfo {
+  constructor({nameSelector, aboutSelector}) {
+    this._nameElement = document.querySelector(nameSelector);
+    this._aboutElement = document.querySelector(aboutSelector);
+  }
+
+  getUserInfo() {
+    return {name: this._nameElement.textContent, about: this._aboutElement.textContent};
+  }
+
+  setUserInfo({name, about}) {
+    this._nameElement.textContent = name;
+    this._aboutElement.textContent = about;
+  };
+}
